d507e4e299c1747537ffb8dcabc49947e5b815c9
C25 <priyankdev1308@gmail.com>
2024-07-30 10:53:36 +0530

    => merege code.

--- lib/api/add_patient_repo/add_patient_repo.dart
+++ lib/api/add_patient_repo/add_patient_repo.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+import 'package:lynerdoctor/api/api_helpers.dart';
+import 'package:lynerdoctor/api/response_item_model.dart';
+import 'package:lynerdoctor/core/constants/request_const.dart';
 import 'package:lynerdoctor/gen/assets.gen.dart';
 import 'package:lynerdoctor/model/productListModel.dart';
 
@@ -23,6 +26,24 @@ class AddPatientRepo {
       return ProductListModel(status: 0, msg: 'Something went wrong', data: []);
     }
   }
+  static Future<ResponseItem> getProductsList() async {
+    ResponseItem result;
+    bool status = true;
+    dynamic data;
+    String msg = "";
 
-
+    final Map<String, int> params = {};
+    final Map<String, String> queryParameters = {
+      RequestParam.service: MethodNames.getProductList,
+      RequestParam.showError: SHOW_ERROR,
+    };
+    String queryString = Uri(queryParameters: queryParameters).query;
+    String requestUrl = ApiUrl.baseUrl + queryString;
+    result = await BaseApiHelper.postRequest(requestUrl, params,
+        passAuthToken: true);
+    status = result.status;
+    data = result.data;
+    msg = result.msg;
+    return ResponseItem(data: data, msg: msg, status: status);
+  }
 }
